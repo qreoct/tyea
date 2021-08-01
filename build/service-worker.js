b5a2ba53ca50a01e3b9e27fb0df48c07be87f50e
qreoct <9080974+qreoct@users.noreply.github.com>
2021-08-01 11:26:40 +0800

    Update
    - Ensure the ordering is okay on gh pages
    - Remove API call and save post locally instead

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be9bc70c5dfc1ac284e20fa8da83b87d.js"
+  "/tyea/precache-manifest.ab6f8c7c0a0ed728a190bd9dd07718fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tyea/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
